617ff90087025e9855dd3d611848983f0153db03
chrise-dcq <dcqmail@163.com>
2015-01-27 23:37:35 +0800

    Signed-off-by: chrise-dcq <dcqmail@163.com>

---
+++ Sort/BubbleSort.c
@@ -0,0 +1,29 @@
+/*冒泡排序
+基本思想：冒泡排序是经过n-1趟子排序完成的，第i趟子排序从第1个数至第n-i个数，
+若第i个数比后一个数大（则升序，小则降序）则交换两数。
+效率分析：若初始序列为正序，则只需进行一趟排序，进行n-1次比较且不移动记录；
+反之初始为逆序时，则需进行n-1趟排序，共n*(n-1)/2次比较和移动。
+一种改进办法：当在某一趟冒泡排序过程中发现没有进行交换操作，就可以结束排序。
+*/
+typedef int ElemType;
+
+void BubbleSort(ElemType array[], int size)  
+{  
+	int i,j;
+    int flag = 0;  
+    for(i = 0; i < size-1; i++)  
+    {  
+        for(j = 0; j < size-1-i; j++)  
+        {  
+            if(array[j] > array[j+1])  
+            {  
+                flag = 1;       //标志位
+                array[j] = array[j] + array[j+1];  
+                array[j+1] = array[j] - array[j+1];  
+                array[j] = array[j] - array[j+1];  
+            }  
+        }  
+        if(flag == 0) 
+			break;  
+    }  
+}  
---
+++ Sort/ShellSort.c
@@ -0,0 +1,25 @@
+
+/*
+shell排序
+基本思想：shell排序是对插入排序的一个改进，它每次排序先根据一个增量获取一个若干子序列，对这这个子序列进行直接插入排序，然后不断的缩小增量扩大子序列的元素数量，直到增量为1的时候子序列就和原先的待排列序列一样了，此时只需要做少量的比较和移动就可以完成对序列的排序了。
+*/
+typedef int ElemType;
+
+void ShellSort(ElemType array[], int length)  
+{  
+    int i,j,increment;  
+	ElemType temp;
+
+    for(increment = length/2; increment > 0; increment /= 2)  
+    {  
+        for(i = increment; i < length; i++)  
+        {  
+            temp = array[i];  
+            for(j = i - increment; j >= 0 && array[j] > temp; j -= increment)  
+            {  
+                array[j + increment] = array[j];  
+            }  
+            array[j + increment] = temp;  
+        }  
+    }  
+} 
